7c06f2285afc938d47780c1e3f4c2e46f9839fd5
Peter Zijlstra <peterz@infradead.org>
2023-03-23 17:41:08 +0000

    sched: Add proxy execution
    A task currently holding a mutex (executing a critical section)
    might find benefit in using scheduling contexts of other tasks
    blocked on the same mutex if they happen to have higher priority
    of the current owner (e.g., to prevent priority inversions).
    
    Proxy execution lets a task do exactly that: if a mutex owner
    has waiters, it can use waiters' scheduling context to
    potentially continue running if preempted.
    
    The basic mechanism is implemented by this patch, the core of
    which resides in the proxy() function. Potential proxies (i.e.,
    tasks blocked on a mutex) are not dequeued, so, if one of them
    is actually selected by schedule() as the next task to be put to
    run on a CPU, proxy() is used to walk the blocked_on relation
    and find which task (mutex owner) might be able to use the
    proxy's scheduling context.
    
    Here come the tricky bits. In fact, owner task might be in all
    sort of states when a proxy is found (blocked, executing on a
    different CPU, etc.). Details on how to handle different
    situations are to be found in proxy() code comments.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    [rebased, added comments and changelog]
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    [Fixed rebase conflicts]
    [squashed sched: Ensure blocked_on is always guarded by blocked_lock]
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    [fix rebase conflicts, various fixes & tweaks commented inline]
    [squashed sched: Use rq->curr vs rq->proxy checks]
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: Rebased, split up, and folded in changes from Juri
     Lelli and Connor O'Brian, added additional locking on
     get_task_blocked_on(next) logic, pretty major rework to better
     conditionalize logic on CONFIG_PROXY_EXEC and split up the very
     large proxy() function - hopefully without changes to logic /
     behavior]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v2:
    * Numerous changes folded in
    * Split out some of the logic into separate patches
    * Break up the proxy() function so its a bit easier to read
      and is better conditionalized on CONFIG_PROXY_EXEC
    v3:
    * Improve comments
    * Added fix to call __balance_callbacks before we call
      pick_next_task() again, as a callback may have been set
      causing rq_pin_lock to generate warnings.
    * Added fix to call __balance_callbacks before we drop
      the rq lock in proxy_migrate_task, to avoid rq_pin_lock
      from generating warnings if a callback was set
    
    TODO: Finish conditionalization edge cases

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1139,7 +1139,9 @@ struct task_struct {
 	struct rt_mutex_waiter		*pi_blocked_on;
 #endif
 
+	struct task_struct		*blocked_proxy;	/* task that is boosting us */
 	struct mutex			*blocked_on;	/* lock we're blocked on */
+	struct list_head		blocked_entry;  /* tasks blocked on us */
 	raw_spinlock_t			blocked_lock;
 
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
